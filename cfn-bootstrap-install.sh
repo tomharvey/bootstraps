cc5ccdb7c3a21ad895833bc8e72b20ce3b6b6ca1
Thomas Harvey <tom@alush.co.uk>
2016-03-27 19:07:15 +0100

    first scripts

---
+++ cfn-bootstrap-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -xe
+
+# Installs the latest CloudFormation tools
+
+apt-get update
+apt-get install -y python-pip python-yaml unattended-upgrades
+pip install pystache
+pip install python-daemon
+
+cd /usr/local/src/
+wget 'https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz'
+tar -xvpf aws-cfn-bootstrap-latest.tar.gz
+cd aws-cfn-bootstrap-1.4/
+python setup.py build
+python setup.py install
+
+cd /etc/init.d/
+ln -s /usr/local/src/aws-cfn-bootstrap-1.4/init/ubuntu/cfn-hup .
+chmod u+x cfn-hup
---
+++ codedeploy-agent-install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Installs the latest code-deploy agent in eu-west-1 region
+# from http://docs.aws.amazon.com/de_de/codedeploy/latest/userguide/how-to-run-agent.html#how-to-run-agent-install-ubuntu
+
+sudo apt-get update
+sudo apt-get install -y python-pip ruby2.0
+sudo pip install awscli
+
+cd /usr/local/src/
+aws s3 cp s3://aws-codedeploy-eu-west-1/latest/install . --region eu-west-1
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
+
+# Check the version
+sudo dpkg -s codedeploy-agent
